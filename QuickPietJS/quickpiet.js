0dcfae1cae3c890e900ac9405da1039149a7457a
Ben <Ben@bens-computer.local>
2010-03-14 18:09:55 -0500

    starting to parse commands

--- QuickPietJS/quickpiet.js
+++ QuickPietJS/quickpiet.js
@@ -1,27 +1,47 @@
 // http://www.agavegroup.com/agWork/prettyForms/
 
 function quickpietjs(commands_id, input_id, output_id, stack_id, log_id) {
-	var commands = $(commands_id)
-	var input = $(input_id)
-	var output = $(output_id)
-	var stack = $(stack_id)
+	var commands = $(commands_id);
+	var input = $(input_id);
+	var output = $(output_id);
+	var stack = $(stack_id);
 	
 	var log_window = $(log_id);
 	var log = function(text) {
-		log_window.val(log_window.val() + text + "\n")
+		log_window.val(log_window.val() + text + "\n");
 	}
 	log.clear = function() {
-		log_window.val('')
+		log_window.val('');
 	}
 	
 	return function() {
-		run(commands, input, output, stack, log)
+		run(commands, input, output, stack, log);
 	};
 }
 
-function run(commands, input, output, stack, log) {
-	log.clear()
-	log("Starting QuickPiet run.")
+function run(commands_win, input_win, output_win, stack_win, log) {
+	log.clear();
+	log("Starting QuickPiet run.");
+	
+	log("Initializing stack.");
+	var the_stack = [];
+	
+	
+	log("Parsing text commands");
+	var commands = commands_win.val().split("\n").filter(function(command) {
+		var trim = jQuery.trim(command);
+		return trim.length > 0 && trim[0] != '#'; 
+	}).map(function(command) {
+		var parts = command.split(' ');
+		
+		if(parts.length == 2 && parts[0] == 'push' && !isNaN(parts[1])) {
+			return function(stack) {
+				stack.push(parts[2]);
+			}
+		}
+		
+	});
+	log("Found " + commands.length + " text commands.");
 	
 	
 	
@@ -30,10 +50,13 @@ function run(commands, input, output, stack, log) {
 	
 	
 	
-	log("Finished QuickPiet run.")
+	
+	
+	
+	log("Finished QuickPiet run.");
 }
 
 
 $().ready(function(e) {
-	$('#run').click(quickpietjs('#commands', '#input', '#output', '#stack', '#log'))
+	$('#run').click(quickpietjs('#commands', '#input', '#output', '#stack', '#log'));
 });
