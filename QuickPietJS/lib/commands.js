f288ae34a73ac1b645cd4bf6edc605ad20447ef7
Benjamin P Lee <leebenjp@gmail.com>
2010-03-24 23:11:45 -0500

    duplicate command

--- QuickPietJS/lib/commands.js
+++ QuickPietJS/lib/commands.js
@@ -10,25 +10,41 @@ Commands = {
 	},
 	
 	pop : function(stack, args) {
-		if(stack.length == 0) {
-			throw new SyntaxError("Invalid POP on empty stack")	
-		}
-		
 		var pops_count = 1
 		
 		if(args && args.length > 0) {
 			args = jQuery.trim(args)
 			
 			if(args.length == 0 || args.match(/[^0-9\s]/) || parseInt(args) == NaN) {
-				throw new SyntaxError("Invalid POP argument")	
+				throw new SyntaxError('Invalid POP argument(s)')	
 			}
 			else {
 				pops_count = parseInt(args)	
 			}
 		}
+		
+		if(pops_count > stack.length) {
+			throw new EvalError('Can not POP from empty stack')
+		}
 
 		for(var i = 0; i < pops_count; i++) {
 			stack.pop()
 		}
+	},
+	
+	duplicate : function(stack, args) {
+		if(stack.length == 0) {
+			throw new EvalError('Can not DUPLICATE from empty stack')
+		}
+		
+		if(args != undefined) {
+			throw new SyntaxError('DUPLICATE does not take arguments')
+		}
+		
+		var val = stack.pop()
+		stack.push(val)
+		stack.push(val)
 	}
+	
+	// next command
 }
--- QuickPietJS/spec/unit/commands.spec.js
+++ QuickPietJS/spec/unit/commands.spec.js
@@ -55,10 +55,12 @@ describe 'Commands'
 	end
 	
 	describe 'pop'
+		before_each
+		  stack.push(10)
+		end
+	
 		it 'should discard top value on stack'
-			stack.push(4)
-			
-			stack.should.eql [4]
+			stack.should.eql [10]
 			
 			Commands.pop(stack)
 			
@@ -66,31 +68,119 @@ describe 'Commands'
 		end
 		
 		it 'should discard top X values from stack if positive integer is given'
-			stack.push(1)
-			stack.push(2)
-			stack.push(3)
+			stack.push(20)
+			stack.push(30)
 			
 			Commands.pop(stack, '2')
 			
-			stack.should.eql [1]
+			stack.should.eql [10]
 		end
 		
 		it 'should error if argument is not a single positive numeric value'
-			stack.push(10)
-			
-			-{ Commands.pop(stack, 'a') }.should.throw_error SyntaxError, 'Invalid POP argument'
-			-{ Commands.pop(stack, '5.6') }.should.throw_error SyntaxError, 'Invalid POP argument'
-			-{ Commands.pop(stack, '-1') }.should.throw_error SyntaxError, 'Invalid POP argument'
+			-{ Commands.pop(stack, 'a') }.should.throw_error SyntaxError, 'Invalid POP argument(s)'
+			-{ Commands.pop(stack, '5.6') }.should.throw_error SyntaxError, 'Invalid POP argument(s)'
+			-{ Commands.pop(stack, '-1') }.should.throw_error SyntaxError, 'Invalid POP argument(s)'
 		end
 		
 		it 'should error if pop is called on an empty stack'
-			-{ Commands.pop(stack)}.should.throw_error SyntaxError, 'Invalid POP on empty stack'
+			stack.pop()
+			-{ Commands.pop(stack) }.should.throw_error EvalError, 'Can not POP from empty stack'
+		end
+		
+		it 'should error if multiple pops are more than stack height'
+		  stack.push(200)
+		  stack.push(200)
+		  stack.push(200)
+		  
+		  -{ Commands.pop(stack, '5') }.should.throw_error EvalError, 'Can not POP from empty stack'
 		end
 		
 		it 'should return undefined'
-			stack.push(100)
-		
 			Commands.pop(stack).should.eql undefined
 		end
+	end
+	
+	describe 'duplicate'
+		it 'should push copy of top value'
+		  stack.push(1)
+		  stack.push(2)
+		  
+		  stack.should.eql [1,2]
+		  
+		  Commands.duplicate(stack)
+		  
+		  stack.should.eql [1,2,2]
+		end
+		
+		it 'should error if stack is empty'
+		  -{ Commands.duplicate(stack) }.should.throw_error EvalError, 'Can not DUPLICATE from empty stack'
+		end
+		
+		it 'should error if arguments are given'
+			stack.push(100)
+			
+		  -{ Commands.duplicate(stack, '') }.should.throw_error SyntaxError, 'DUPLICATE does not take arguments'
+		  -{ Commands.duplicate(stack, ' ') }.should.throw_error SyntaxError, 'DUPLICATE does not take arguments'
+		  -{ Commands.duplicate(stack, 'a') }.should.throw_error SyntaxError, 'DUPLICATE does not take arguments'
+		  -{ Commands.duplicate(stack, '1') }.should.throw_error SyntaxError, 'DUPLICATE does not take arguments'
+		end
+		
+		it 'should return undefined'
+		  stack.push(100)
+		  
+		  Commands.duplicate(stack).should.eql undefined
+		end
+	end
+	
+	describe 'roll'
+	
+	end
+	
+	describe 'in'
+	  
+	end
+	
+	describe 'out'
+	  
+	end
+	
+	describe 'add'
+	  
+	end
+	
+	describe 'subtract'
+	  
+	end
+	
+	describe 'multiply'
+	  
+	end
+	
+	describe 'divide'
+	  
+	end
+	
+	describe 'mod'
+	  
+	end
+	
+	describe 'not'
+	  
+	end
+	
+	describe 'greater'
+	  
+	end
+	
+	describe 'end'
+	  
+	end
+	
+	describe 'goto'
+	  
+	end
+	
+	describe 'assert'
+	  
 	end
 end
